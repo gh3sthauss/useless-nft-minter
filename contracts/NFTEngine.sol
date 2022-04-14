089122024630aa27eb91454a2e69649f7dbfede2
bunyam1n <panophilic@gmail.com>
2022-04-14 19:02:08 +0300

    base

---
+++ contracts/NFTEngine.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BERAT is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("BERAT", "BRT") {}
+
+    function safeMint() public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(_msgSender(), tokenId);
+    }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import { noSSR } from 'next/dynamic'
+import ReactNoSsr from 'react-no-ssr'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ReactNoSsr> <Component {...pageProps} /></ReactNoSsr>
+   
 }
 
 export default MyApp
