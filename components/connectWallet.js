e71d11e6d8a63e34a05322a9da47ddd50aca3742
bunyam1n <panophilic@gmail.com>
2022-04-14 20:10:27 +0300

    some other really minor changes

--- components/connectWallet.js
+++ components/connectWallet.js
@@ -60,14 +60,7 @@ function ConnectWallet() {
         }
       });
     } else {
-      setStatus(
-        <p>
-          <a target="_blank" href={`https://metamask.io/download.html`}>
-            You must install Metamask, a virtual Ethereum wallet, in your
-            browser.
-          </a>
-        </p>
-      );
+    
     }
   }
   const connectWalletPressed = async () => {
